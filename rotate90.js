52b22a236309caf058b00bf263e8effbff43a0ff
dgbailey <bailey.dust@gmail.com>
2020-03-19 14:22:27 -0700

    is palendrome LL

--- rotate90.js
+++ rotate90.js
@@ -1,7 +1,8 @@
 function rotateImage(a) {
-	for (let i = 0; i < a.length; a++) {
+	for (let i = 0; i < a.length; i++) {
 		//transpose array over center diagonal
-		for (let j = 0; j < a.length; j++) {
+		let length = a[0].length - 1;
+		for (let j = 0; j < i; j++) {
 			let temp = a[i][j];
 			a[i][j] = a[j][i];
 			a[j][i] = temp;
@@ -17,8 +18,14 @@ function rotateImage(a) {
 			a[i][length - j] = temp;
 		}
 	}
+
+	return a;
 }
 
 //without any prior knowledge of array manipulations this was difficult.
 //without knowledge of transposing this was difficult
 //inplace made this difficult as well
+//rotation then depens on number of times run
+let img = [ [ 1, 2, 3, 6 ], [ 4, 5, 6, 8 ], [ 7, 8, 9, 7 ], [ 0, 0, 0, 8 ] ];
+console.log(rotateImage(img));
+console.log(rotateImage(img));
