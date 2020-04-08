6e7754eaf9b8dbc1323d5f3fc8eb2e0fe79fc4f4
dgbailey <bailey.dust@gmail.com>
2020-04-07 18:34:32 -0700

    permutation notes

--- permutations.js
+++ permutations.js
@@ -10,20 +10,20 @@ function getPerms() {
 			let newPrefix = prefix + suffix[i];
 			let newSuffix = suffix.slice(0, i) + suffix.slice(i + 1);
 			allPermutations(newPrefix, newSuffix);
-			// if (perm) {
-			// 	p.push(perm);
-			// }
 		}
-
-		//first all n combos
-		//d
-		//do //g
-		//dog //'' ~
-		//d
-		//nae ~
-		//naem *
-		//nm
 	}
 }
 
 getPerms();
+//splitting into prefex and suffix was the tough part here
+//also following the recursion on each level was tough
+//following the return of recursive calls was initually printing undefined as a base case returned to a previous stack frame
+//splitting followed from logic
+//ok first all perms starting with prefix d
+//then with do
+//then with dog
+//return
+//ok now with dg suffix o
+//ok now with dgo suffix ''
+//return
+//ok now with o suffix 'dg' and so on
