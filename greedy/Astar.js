2a595b41999aebf53c07f08ebd3cecc5937fd44b
dgbailey <bailey.dust@gmail.com>
2020-04-15 15:33:06 -0700

    starting primary search algo

--- greedy/Astar.js
+++ greedy/Astar.js
@@ -28,24 +28,49 @@ function _filterBoundaries(swapsArray, ...args) {
 	});
 }
 
-function _nextBoardStates(currentState, goalState) {
-	//find null
-	//find next moves
-	//make moves
-	//return matrices
+function _nextBoardState(currentState, goalState) {
+	//returns next optimal matrix
 	let nullCoords = _findBlankTile(currentState);
 	let rowLimit = currentState.length;
 	let colLimit = currentState[0].length;
 	let filteredSwapArray = _filterBoundaries(_createSwapsArray(nullCoords, currentState), rowLimit, colLimit);
-	//we make our decision here which state to push into queue
+	let newBoardStates = _implementSwaps(filteredSwapArray, currentState, nullCoords);
+	return _calcMinHeuristic(newBoardStates, goalState); //find  min g(n) + h (n)
+}
+
+function _calcMinHeuristic(newBoardStates, goalState) {
+	//assumption here is that every g(n) is constant (1)
+	//optimum then depends on underestimate of h(n)
+
+	let heuristicScrores = [];
+	let lookupTable = _mDistanceLookup(goalState);
+	for (let newBoardState of newBoardStates) {
+		let h = _hHeuristic(newBoardState, lookupTable, _manhattan);
+		let g = _gHeuristic();
+		heuristicScrores.push(h + g);
+		return newBoardStates[maxIndex(heuristicScrores)];
+	}
+
+	function maxIndex(arr) {
+		let min = Infinity;
+		let index;
+		for (let i = 0; i < arr.length; i++) {
+			if (value < min) {
+				min = value;
+				index = i;
+			}
+		}
+		return index;
+	}
 }
 
 function _implementSwaps(filteredSwapArray, currentMatrixState, nullCoords) {
+	//returns array of new path matrices
 	filteredSwapArray.map((fs) => {
 		let copyCurrentState = currentMatrixState.map((r) => [ ...r ]);
 		let tmp = copyCurrentState[(nullCoords[0], nullCoords[1])];
 		copyCurrentState[(nullCoords[0], nullCoords[1])] = copyCurrentState[(fs[0], fs[1])];
-		copyCurrentState[(fs[0], fs[1])] = temp;
+		copyCurrentState[(fs[0], fs[1])] = tmp;
 		return copyCurrentState;
 	});
 }
@@ -61,13 +86,29 @@ function _createSwapsArray(blankTileCoords, matrix) {
 	});
 }
 
-function _chooseNextBestPath() {
-	//how?
-	//need min heuristic score for each next possible path
+function _manhattan(a, b) {
+	return Math.abs(a - b);
 }
 
-function _hHeuristic(nextState, goalState) {}
-function _gHeuristic(currentState, nextState) {}
+function _hHeuristic(newBoardState, lookupTable, heuristic) {
+	//return arr of H scores for each possibility
+	let hScores = [];
+	for (let i = 0; i < newBoardStates.length; i++) {
+		for (let j = 0; j < newBoardStates.length; j++) {
+			let boardValue = newBoardState[i][j];
+			let [ goalRow, goalCol ] = lookupTable[boardValue];
+			let hRow = heuristic(goalRow, i);
+			let hCol = heuristic(goalCol, j);
+			hScores.push(hRow + hCol);
+		}
+	}
+	return hScores.reduce(sum, (v) => sum + v);
+}
+
+function _gHeuristic(currentState = null, nextState = null) {
+	//returns one given that any variant of the sliding puzzle can only differ by manhattan of 1
+	return 1;
+}
 
 function _findBlankTile(matrix) {
 	for (let i = 0; i < matrix.length; i++) {
