0f14f57be601c23f92235a3aaabe83e57ecb9d18
dgbailey <bailey.dust@gmail.com>
2020-05-14 19:18:17 -0700

    another test rep

--- async.js
+++ async.js
@@ -1,14 +1,14 @@
 async function getD() {
-  try {
-    let i = await fetch("https://dog.ceo/api/breeds/image/random");
-    let j = await i.json();
-    console.log(j);
-  } catch (err) {
-    console.log(err);
-  }
+	try {
+		let i = await fetch('https://dog.ceo/api/breeds/image/random');
+		let j = await i.json();
+		console.log(j);
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 getD();
-
+//testing
 //this is a somewhat concise pattern for fetching data,
 //however important to consider exactly where you would dispatch events based on success or errors
