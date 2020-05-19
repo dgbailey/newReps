8e0287d94012394a96c6795da72116f57c132d80
dgbailey <bailey.dust@gmail.com>
2020-05-18 20:47:12 -0700

    updating async

--- async.js
+++ async.js
@@ -1,15 +1,15 @@
-async function getD(){
-    try{
-        let i = await fetch('https://dog.ceo/api/breeds/image/random');
-        let j = await i.json();
-        console.log(j);
-    }
-    catch(err)
-        {console.log(err) }
-    }
+async function getD() {
+  try {
+    let i = await fetch("https://dog.ceo/api/breeds/image/random");
+    let j = await i.json();
+    console.log(j);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-
- getD();
+getD();
 
 //this is a somewhat concise pattern for fetching data,
+//new
 //however important to consider exactly where you would dispatch events based on success or errors
