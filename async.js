a1397a325f6d4bea45568178c80901241c43903c
dgbailey <bailey.dust@gmail.com>
2020-05-29 20:29:40 -0700

    public commitment to starting this next week

--- async.js
+++ async.js
@@ -1,19 +1,4 @@
 async function getD() {
-<<<<<<< HEAD
-  try {
-    let i = await fetch("https://dog.ceo/api/breeds/image/random");
-    let j = await i.json();
-    console.log(j);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-getD();
-
-//this is a somewhat concise pattern for fetching data,
-//new
-=======
 	try {
 		let i = await fetch('https://dog.ceo/api/breeds/image/random');
 		let j = await i.json();
@@ -26,5 +11,5 @@ getD();
 getD();
 //testing
 //this is a somewhat concise pattern for fetching data,
->>>>>>> d3829b8a11f3c9fa7deca12af314d2e61067af8a
 //however important to consider exactly where you would dispatch events based on success or errors
+//starting this next week
