9da2a0cd17571f204a4466cb80afe552bf1b2b53
dgbailey <bailey.dust@gmail.com>
2020-06-03 16:43:28 -0700

    invertTree

---
+++ third/sameTree.js
@@ -0,0 +1,32 @@
+//same tree
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    //simultaneous traversal
+    //preorder?
+    
+    if(!p && !q){
+        return true
+    }
+    if(!(p && q)){
+        return false
+    }
+    if(p.val !== q.val){
+        return false
+    }
+    
+
+    return isSameTree(p.left,q.left) && isSameTree(p.right, q.right)
+};
